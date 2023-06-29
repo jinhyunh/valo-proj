7418cac63055f00df517911ca9f8353024ca6315
Jinhyun Hwang <jinhyunh0808@gmail.com>
2023-06-29 11:41:49 -0400

    Figure out next steps, need to organize work into different files

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 import valo_api.endpoints
 import json
+import pandas as pd
 
 
 def get_mmr_history_by_name():
@@ -11,7 +12,7 @@ def get_match_details():
 
 
 def get_match_history():
-    return valo_api.endpoints.get_match_history_by_name_v3("na", "Devendro", "3876", game_mode="Competitive")
+    return valo_api.endpoints.get_match_history_by_name_v3("na", "oxyjin02", "LOFT", size="1", map="", game_mode="Competitive")
 
 
 def get_json(obj):
@@ -19,8 +20,32 @@ def get_json(obj):
 
 
 if __name__ == '__main__':
+
     match_history = get_match_history()
     match_history_dict = get_json(match_history)
+    """
     with open("prac.json", "w") as outfile:
         json.dump(match_history_dict, outfile, indent=4)
+    """
+    print(match_history_dict[0]["metadata"])
+    df = pd.read_json('prac.json')
+    df1 = pd.DataFrame(data=match_history_dict[0]["metadata"], index=[0])
+    print(df1)
 
+    """
+    It looks like there is 3 distinct types of functions needed at the moment,
+    the get_json which reads in the dict and formats it.
+    
+    The functions to format the data(in dict/json) into DataFrame
+    
+    The functions that make endpoint calls easier
+    """
+
+    """
+    the "rounds" key in json is a list, i have to loop through list, and for each index of list,
+    i have to get all the kill events per round
+    
+    need a counter for round, 
+    
+    later i can just add a column to every row for map, match id, etc.
+    """
